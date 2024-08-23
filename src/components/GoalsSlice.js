7a21799f920ae777ca58c8bc55a2e440555a9961
LukeJSimmons <lucasjsimmons@yahoo.com>
2024-08-22 19:18:03 -0500

    New Browser Goals Bug: Attempt 1

--- src/components/GoalsSlice.js
+++ src/components/GoalsSlice.js
@@ -5,7 +5,7 @@ import { LoadData } from "../SaveHandler";
 
 const goalsSlice = createSlice({
     name: 'goals',
-    initialState: LoadData().goals ? LoadData().goals : [],
+    initialState: LoadData().goals !== null || undefined ? LoadData().goals : [],
     reducers: {
         addGoal: (state, action) => {
             return [...state, action.payload]
