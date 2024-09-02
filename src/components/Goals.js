bd7fc91078e5ff3e99b41d7c59f5726836cdfb64
LukeJSimmons <lucasjsimmons@yahoo.com>
2024-09-02 07:32:50 -0500

    Fix Small Errors

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,7 +1,6 @@
 import "./Goals.css";
 
-import store from "../store";
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Goal from "./Goal";
 import { useSelector } from "react-redux";
